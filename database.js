89083ab88c44ed6026e990060b01ae011b1c3fcf
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-11-05 20:58:46 -0400

    added funcitons

---
+++ database.js
@@ -0,0 +1,24 @@
+'use strict';
+function createUser(){
+
+}
+
+function deleteUser(username){
+
+}
+
+function createPost(){
+
+}
+
+function updatePost(){
+
+}
+
+function deletePost(){
+
+}
+
+function getMSGLog(id){
+
+}
