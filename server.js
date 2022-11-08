9e3b64fb30301256ed99f658ba21194c814bc542
johnrandd241 <112962592+johnrandd241@users.noreply.github.com>
2022-11-08 13:25:03 -0500

    port

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }))
