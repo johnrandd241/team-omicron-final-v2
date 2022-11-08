4657ee67fd27febf3c71542b07352d20d981380a
johnrandd241 <112962592+johnrandd241@users.noreply.github.com>
2022-11-08 13:13:58 -0500

    added some things

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+//John made - for his login page to redirect & such
  
+//to redirect from index to login page
+app.post("/login", (req, res) => {
+    res.sendFile(__dirname + "/login-page.html");
+});
+
+//to redirect from index to register page
+app.post("/register", (req, res) => {
+    res.sendFile(__dirname + "/register.html");
+});
+
+
+
+
+//John made - for his login page to redirect & such
+
