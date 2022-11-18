022611057fd098d72493933aaa6756731e485153
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-11-18 16:14:22 -0500

    Added barebones database connection functionality

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 'use strict';
-function createUser(username, name){
+/* function createUser(username, name){
 
 }
 
@@ -50,3 +50,34 @@ function createMSGLog(postid){
 function updateMSGLog(logid){
 
 }
+ */
+
+//Database connection code
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+//Load and intialize library
+const pgp = require('pg-promise')({//Intilization Options
+});
+//Connection details prep
+const cn = 'postregs://username:password@host:port/database';
+//Creates new database instance baserd on connection details
+const db = pgp(cn);
+//Export db for shared use
+module.exports = db;
