a019d353c8181d445b394eccaa8a05be63b8f80c
John Steenbruggen Jr <jsteenbrugge@umass.edu>
2022-11-19 18:32:59 -0500

    made it so the events, people, and records tab spawn in programatically

--- main.js
+++ main.js
@@ -1,15 +1,63 @@
 let cur_section = 'events'; // we open the events section by default, this variable keeps track of which section we have open
+const POSTS_PER_ROW = 3;
+const DUMMY_POST = {title: 'This is the title of the post', img_src: 'www.com', desc: 'This is the description of the post', user: '123', date: '10/5/22'};
+
+// given the unique id for a user, get the user object from the database
+function getUserFromID(user_id) {
+    return {id: user_id, name: 'dummy data', friends: [], posts: [], messages: []};
+}
 
 // if search button is pressed and the search text is blank, just return everything from that section, sorted by date
 function search(keywords) {
-    // obtain stuff from database (based on cur_section and search text)
+    // obtain stuff from database (based on cur_section and keywords)
     // programatically create the html that displays the post (the response from the database)
     // render the elements into the page
+    // at this point, all of the 'posts' should be in some kind of array, all we gotta do now is render them
+    let arr = [DUMMY_POST, DUMMY_POST, DUMMY_POST, DUMMY_POST, DUMMY_POST, DUMMY_POST]; 
+    // create the base element for the posts (this container holds the rows and columns and what not)
+    let container = document.createElement('div');
+    container.classList.add('container'); // specify the fact it is a container
+    container.classList.add('mt-5'); // set the size
+    // while there are still posts to render
+    while (arr.length > 0) {
+        // create the element for this row of posts
+        let cur_row = document.createElement('div');
+        cur_row.classList.add('row'); // specify that it is a row
+        cur_row.classList.add('gx-5'); // set the sizing
+        // go through them POSTS_PER_ROW at a time (i think 2 posts per row is good)
+        for (post_data in arr.slice(0, POSTS_PER_ROW)) {
+            console.log(post_data);
+        }
+        arr.slice(0, POSTS_PER_ROW).forEach(post_data => {
+            // create the column cell for the post
+            let cur_col = document.createElement('div');
+            cur_col.classList.add('col');
+            cur_col.classList.add('p-' + POSTS_PER_ROW);
+            cur_col.classList.add('bg-white');
+            let title_element = document.createElement('h2');
+            title_element.innerHTML = post_data.title;
+            let img_element = document.createElement('img');
+            img_element.src = post_data.img_src;
+            let desc_element = document.createElement('p');
+            desc_element.innerHTML = post_data.desc;
+            let meta_element = document.createElement('p');
+            meta_element.innerHTML = 'Posted by ' + getUserFromID(post_data.user).name + ' on ' + post_data.date;
+            cur_col.appendChild(title_element);
+            cur_col.appendChild(img_element);
+            cur_col.appendChild(desc_element);
+            cur_col.appendChild(meta_element);
+            cur_row.appendChild(cur_col);
+            arr.shift();
+        });
+        container.appendChild(cur_row);
+    }
+    document.getElementById('page').textContent = '';
+    document.getElementById('page').appendChild(container);
 }
 
 // runs when message tab is clicked
 function messages() {
-    
+
 }
 
 // runs when profile tab is clicked
@@ -60,4 +108,7 @@ window.onload = function() {
             }
         });
     });
+
+    // sort posts by date into the page
+    search('');
 };
