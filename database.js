1917ee802ea761be63c287d9594fe6111f37e28c
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-11-20 19:53:56 -0500

    Connection Testing

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 export const DUMMY_POST = {title: 'This is the title of the post', img_src: 'www.com', desc: 'This is the description of the post', user: 'ch4rl3sd4rw1n', date: '10/5/22', id: 129839753759869, type: 'event'};
 export const DUMMY_USER = {username: 'ch4rl3sd4rw1n', name: 'Charles Darwin', img_src: 'https://upload.wikimedia.org/wikipedia/commons/2/2e/Charles_Darwin_seated_crop.jpg', bio: 'this is my biography, i have no family no friends no money no home no gall bladder and im starving and my dog died', posts: [123, 456, 789, 1234], friends: ['lemonman1', 'eggace4848', 'Hyn7eff']};
 
+
 export function registerUser(username) {
     // basically, create an object like DUMMY_USER and store it in the database
     // here we need to create an entry for this new user in our database
