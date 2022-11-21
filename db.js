730884a8e45152b265d3aaf1ef1c25d35b345d43
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-11-20 21:59:57 -0500

    Connection Testing

--- db.js
+++ db.js
@@ -6,8 +6,6 @@ const pgp = require('pg-promise')(initOptions);
 //Connection Setup
 const cn = 'postgres://username:password@host:port/database';
 //Create Database object
-const db = pgp(cn);
+export const db = pgp(cn);
 
-module.exports = {
-    db
-};
+//module.exports = db;
