51ea8e378ae8fdcee7b8ef93ca09bf9a51b598d1
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-11-20 19:40:47 -0500

    Added More Functionality

--- database.js
+++ database.js
@@ -19,12 +19,17 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   client.end();
 });
 
-//Load and intialize library
+/* //Load and intialize library
 const pgp = require('pg-promise')({//Intilization Options
 });
 //Connection details prep
-const cn = 'postregs://vuoyxzmwyzkavq:a4f5cc059f60dedb000bd8f405ca897fe3809c3632303a5d0df28cac882e4b20@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d4l2404nskei9g';
+const cn = 'postgres://username:password@host:port/database';//'postregs://vuoyxzmwyzkavq:a4f5cc059f60dedb000bd8f405ca897fe3809c3632303a5d0df28cac882e4b20@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d4l2404nskei9g';
 //Creates new database instance baserd on connection details
 const db = pgp(cn);
 //Export db for shared use
 module.exports = db;
+
+db.one(new PQ('SELECT * FROM users'))
+    .then(chat => {console.log(chat);})
+    .catch(error =>{//handle errors
+    }); */
---
+++ messages.js
@@ -0,0 +1,126 @@
+import db from database.js;
+export function renderChat(div, chatID){
+    if(typeof(chatID) === "undefined"){
+        let empty = document.createElement('div');
+        empty.innerHTML = "Please Select a chat from the list on the left";
+        div.appendChild(empty);
+        return;
+    }
+    let msg={};
+    //Request chat data from database
+    const getChat = new PQ({text:'SELECT * FROM chatTable where ID = $chatID'});
+    db.one(getChat)
+        .then(chat => {msg=JSON.parse(chat);})
+        .catch(error =>{//handle errors
+        });
+    
+    for(let msg in chat){
+        let bubble = document.createElement('div');
+        bubble.classList.add("d-flex mb-4");
+        if(curUser/*some global for ID of whos signed in*/ === msg.user){
+            bubble.classList.add('justify-content-start');
+        }else{
+            bubble.classList.add('justify-content-end');
+        }
+        let uinfo = document.createElement('div');
+        uinfo.innerHTML = msg.user;
+        //uinfo.classList.add()
+        let msgC = document.createElement('div');
+        msgC.classList.add("msg_container");
+        msgC.innerHTML = msg.text;
+        let time = document.createElement('span');
+        time.classList.add('msg_time');
+        time.innerHTML = msg.date;
+        msgC.appendChild(time);
+        bubble.appendChild(uinfo);
+        bubble.appendChild(msgC);
+    }
+
+}
+
+function renderConvos(div){
+    let convos = {};
+    let list = document.createElement('ul');
+    list.classList.add('contacts');
+    const getConvos = new PQ('SELECT messages FROM UserTable WHERE Username = $curUser');
+    db.any(getConvos)
+        .then(convo => {convo=JSON.parse(convos);})
+        .catch(error =>{//handle errors
+        });
+    for(let c in convos){
+        let item = document.createElement('li');
+        item.id = c.friend;
+        //item.classList.add('')
+        let card =  document.createElement('div');
+        card.classList.add('d-flex bd-highlight');
+        let pic = document.createElement('div');
+        pic.classList.add('img_cont');
+        let image = document.createElement('img');
+        image.src = './stockUserPhoto.jpeg';
+        pic.appendChild(image);
+        card.appendChild(pic);
+        let friend = document.createElement('div');
+        friend.classList.add('user_info');
+        let name = document.createElement('span');
+        name.innerHTML = c.friend;
+        friend.appendChild(name);
+        card.appendChild(friend);
+        item.appendChild(card);
+        div.appendChild(item);
+    }
+}
+
+
+export function message(div, chatID){
+    let chats = document.createElement('div');
+    chats.classList.add('card-body contacts_body');
+    renderConvos(chats);
+    let curchat = document.createElement('div');
+    curchat.classList.add("card-body msg_card_body");
+    renderChat(curchat, chatID);
+
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.appendChild(curchat);
+    //Message entry field
+    let input = document.createElement('div');
+    input.classList.add('card-footer');
+    let ingroup = document.createElement('div');
+    ingroup.classList.add('input-group');
+    let msgEntry = document.createElement('textarea');
+    msgEntry.classList.add('form-control type_msg');
+    msgEntry.placeholder = "Type a message";
+    ingroup.appendChild(msgEntry);
+    let send = document.createElement('div');
+    send.classList.add('input-group-append');
+    let butt = document.createElement('span');
+    butt.classList.add('input-group-text send_btn');
+    let innerButt = document.createElement('i');
+    innerButt.classList.add('fas fa-location-arrow');
+    butt.appendChild(innerButt);
+    send.appendChild(butt);
+    ingroup.appendChild(send);
+    input.appendChild(ingroup);
+    card.appendChild(input);
+    //End msg entry field creation and addition to main div
+    //Current Chat styling div finalization
+    let RHS = document.createElement('div');
+    RHS.classList.add("col-md-8 col-xl-6 chat");
+    RHS.appendChild(card);
+    
+    //List of chats style finalization
+    let LHS = document.createElement('div');
+    LHS.classList.add('col-md-4 col-xl-3 chat');
+    let LHS2 = document.createElement('div');
+    LHS2.classList.add('card mb-sm-3 mb-md-0 contacts_card');
+    LHS2.appendChild(chats);
+    LHS.appendChild(LHS2);
+
+    //Div to split screen into chats and currently opened chat
+    let split = document.createElement('div');
+    split.classList.add('row justify-content-center h-100');
+    split.appendChild(LHS);
+    split.appendChild(RHS);
+    div.classList.add('constiner-fluid h-100');
+    div.appendChild(split);
+}
