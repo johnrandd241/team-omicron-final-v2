6a18268d24675deae082c0f1deedf0a1618b186b
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-11-20 21:46:06 -0500

    Connection Testing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { ParameterizedQuery } from './node_modules/pg-promise';
+import { ParameterizedQuery } from './node_modules/pg-promise/lib/types/parameterized-query.js';
 import {getDB} from './db.js';
 const {db, pgp} = getDB();
 console.log(Database.DUMMY_POST);
