cd3c6341804562e84e3469945d90dc941a5310e7
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-12-09 15:33:37 -0500

    vgyueihopy

--- messages.js
+++ messages.js
@@ -21,7 +21,7 @@ export async function message(chatID){
                 console.log("API call failed. Exiting function renderConvos. Setting Div Text to Error.");
                 chats.innerHTML = "Error API called failed!\nPlease try again later.";
             }
-            convos = response.json();
+            convos = resp.json();
         });
     for(let c in convos){
         let item = document.createElement('li');
@@ -62,7 +62,7 @@ export async function message(chatID){
                 console.log("API call failed. Exiting function renderConvos. Setting Div Text to Error.");
                 chats.innerHTML = "Error API called failed!\nPlease try again later.";
             }
-            chat = response.json();
+            chat = resp.json();
         });
     
     for(let msg in chat){
