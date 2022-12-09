303c81e28c122710e1ff21130ed3fbeba4440349
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-12-08 20:09:12 -0500

    Database connection

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const ports = 5432;
 const username = 'vuoyxzmwyzkavq';
 const password = 'a4f5cc059f60dedb000bd8f405ca897fe3809c3632303a5d0df28cac882e4b20';
 const database = 'd4l2404nskei9g';
-const cn = `postgres://${username}:${password}@${host}:${ports}/${database}`;
+const cn = "postgres://vuoyxzmwyzkavq:a4f5cc059f60dedb000bd8f405ca897fe3809c3632303a5d0df28cac882e4b20@ec2-34-230-153-41.compute-1.amazonaws.com:5432/d4l2404nskei9g";
 // Creating a new database instance from the connection details:
 const db = pgp(cn);
 // Exporting the database object for shared use:
