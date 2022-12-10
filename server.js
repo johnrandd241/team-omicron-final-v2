77069f7bee50ace97b15b4b3eac951d7c66890cd
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-12-10 18:43:16 -0500

    few

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.post("/update/msg", (req, res)=>{
 
 app.get("/GetMsgFromUser", (req, res)=>{
     const user = req.query.user;
-    const q = `SELECT messages FROM users WHERE username = '${user}';`;
-    console.log("attempting this query: " + q);
+    const q = `SELECT messages FROM users WHERE username = "$(user)";`;
+    console.log("attempting this query: " + q +user);
     db.any(q)
     .then(resp => {
         console.log("got this response (THTHTHT): ");
