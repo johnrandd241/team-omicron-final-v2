77af54764150e7377d16ec9afd915f735e5d4a39
johnrandd241 <112962592+johnrandd241@users.noreply.github.com>
2022-12-09 22:27:06 -0500

    brokelog, fixing atm

--- login.js
+++ login.js
@@ -22,7 +22,7 @@ document.getElementById('loginButton').addEventListener('click', (e) => {
              const result = await sent.json();
              console.log(result);
              DO().then(function(result){
-                if(result.isAuth){
+                if(result != undefined){
                     let theUser = JSON.parse(window.localStorage.getItem('user'));
                     theUser.username = result.username;
                     theUser.password = result.password;
