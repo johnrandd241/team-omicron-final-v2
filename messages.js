60410a1633e106de1b31707f3cd3cdd228603a3a
Swerdnacs <112971483+Swerdnacs@users.noreply.github.com>
2022-12-10 18:33:55 -0500

    Exported and implemented currUser

--- messages.js
+++ messages.js
@@ -1,5 +1,5 @@
 import {currentuser} from "./main.js";
-let logged_user = currentuser.username;
+let logged_user = currentUser.username;
 export async function message(chatID){
     let div = document.getElementById('page');
     div.innerHTML = "";
