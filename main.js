a712f751c00dc7466abfe310fcb9796674a53eb3
John Steenbruggen Jr <jsteenbrugge@umass.edu>
2022-12-10 12:53:02 -0500

    fixes

--- main.js
+++ main.js
@@ -34,10 +34,13 @@ async function getColumnForPost(post_data) {
     let desc_element = document.createElement('p');
     desc_element.innerHTML = post_data.postdescription; // .substring(0, 100); // desc changed to postdescription
     let meta_element = document.createElement('p');
+    meta_element.innerHTML = "blank for now";
+    /*
     let userfetch = await fetch("/users/get?userid=" + post_data.userid);
     userfetch.json().then(delivered => {
         meta_element.innerHTML = 'Posted by ' + delivered[0].nameofuser + ' on ' + post_data.creationdate; // creationdate changed to date
     });
+    */
     meta_element.style.textDecoration = 'underline';
     if (post_data.postid != 0) {
         meta_element.addEventListener('click', () => {
