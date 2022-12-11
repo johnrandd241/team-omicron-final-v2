d55a48b41a8abfd1358ac3ea732a09e8bda4e62f
John Steenbruggen Jr <jsteenbrugge@umass.edu>
2022-12-11 18:14:33 -0500

    eieje

--- main.js
+++ main.js
@@ -94,11 +94,13 @@ async function search(keywords) {
         // console.log("about to view these posts");
         // console.log(arr);
         // arr.forEach(e => console.log(score(e.tags)));
+        /*
         if (keywords.length > 0) {
             arr = arr.sort((a, b) => {
                 return score(b.tags) - score(a.tags);
             });
         }
+        */
         // create the base element for the posts (this container holds the rows and columns and what not)
         let container = document.createElement('div');
         container.classList.add('container'); // specify the fact it is a container
